58c3e2090d47a1dddc002cc0f72dfd3e82cfba73
Mads Churchill <madschurchill@gmail.com>
2025-10-19 10:38:37 +0200

    Filter incidents for Jinja rendering

--- backend/app/repo.py
+++ backend/app/repo.py
@@ -1,1 +1,16 @@
+from contextlib import contextmanager
+from sqlmodel import SQLModel, Session, create_engine
+
+
+engine = create_engine("sqlite:///./game.db", echo=False)
+
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+
+@contextmanager
+def get_session():
+    with Session(engine) as session:
+        yield session
 
